8fac97cfd926a92ce152a4177e5f44454dede966
yusei-wy <yusei.kasa@gmail.com>
2022-10-29 12:52:10 +0900

    add: user repository

---
+++ domain/repository/src/user_repository.rs
@@ -0,0 +1,15 @@
+use async_trait::async_trait;
+use entity::{User, UserId};
+
+#[mockall::automock]
+#[async_trait]
+pub trait UserRepository {
+    async fn save(&self, user: &User) -> anyhow::Result<()>;
+    async fn get_by_ids(&self, ids: &[UserId]) -> anyhow::Result<Vec<User>>;
+}
+
+pub trait ProvideUserRepository {
+    type Repository: UserRepository;
+
+    fn provide(&self) -> &Self::Repository;
+}
