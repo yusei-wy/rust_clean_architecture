7043497419e139ae2faac7d2f0532a3959fb68c4
yusei-wy <yusei.kasa@gmail.com>
2022-11-01 20:24:25 +0900

    add: grpc handler

--- infra/grpc_handler/src/convert.rs
+++ infra/grpc_handler/src/convert.rs
@@ -1,10 +1,13 @@
 use anyhow::{self, Context};
 
-use entity::User;
+use entity::{User, UserId};
 use error::AppError;
 use usecase::CreateUserCommand;
 
-use crate::user::v1::{CreateUserRequest, CreateUserResponse, User as PbUser};
+use crate::user::v1::{
+    CreateUserRequest, CreateUserResponse, GetUsersByIdsRequest, GetUsersByIdsResponse,
+    User as PbUser,
+};
 
 impl TryFrom<CreateUserRequest> for CreateUserCommand {
     type Error = anyhow::Error;
@@ -35,5 +38,21 @@ impl From<User> for PbUser {
             id: user.id().to_string(),
             name: user.name().to_string(),
         }
+    }
+}
+
+impl TryFrom<GetUsersByIdsRequest> for Vec<UserId> {
+    type Error = anyhow::Error;
+
+    fn try_from(request: GetUsersByIdsRequest) -> Result<Self, Self::Error> {
+        let GetUsersByIdsRequest { ids } = request;
+        ids.into_iter().map(|id| id.parse()).collect()
+    }
+}
+
+impl From<Vec<User>> for GetUsersByIdsResponse {
+    fn from(users: Vec<User>) -> Self {
+        let users = users.into_iter().map(From::from).collect();
+        GetUsersByIdsResponse { users }
     }
 }
--- infra/grpc_handler/src/lib.rs
+++ infra/grpc_handler/src/lib.rs
@@ -1,9 +1,70 @@
-mod convert;
-
-pub use convert::*;
-
 pub mod user {
     pub mod v1 {
         tonic::include_proto!("user.v1");
+    }
+}
+
+mod convert;
+
+use app_context::AppContext;
+use derive_getters::Getters;
+use derive_new::new;
+use error::AppError;
+use tonic::{Request, Response, Status};
+use user::v1::user_service_server::UserService;
+use user::v1::{
+    CreateUserRequest, CreateUserResponse, GetUsersByIdsRequest, GetUsersByIdsResponse,
+};
+
+#[derive(new, Getters)]
+pub struct UserServiceHandler {
+    ctx: AppContext,
+}
+
+#[tonic::async_trait]
+impl UserService for UserServiceHandler {
+    async fn create_user(
+        &self,
+        request: Request<CreateUserRequest>,
+    ) -> Result<Response<CreateUserResponse>, Status> {
+        let cmd = request
+            .into_inner()
+            .try_into()
+            .map_err(|e| handle_error(e))?;
+
+        let user = usecase::create_user(self.ctx(), cmd)
+            .await
+            .map_err(|e| handle_error(e))?;
+
+        Ok(Response::new(user.into()))
+    }
+
+    async fn get_users_by_ids(
+        &self,
+        request: Request<GetUsersByIdsRequest>,
+    ) -> Result<Response<GetUsersByIdsResponse>, Status> {
+        let ids = request
+            .into_inner()
+            .try_into()
+            .map_err(|e| handle_error(e))?;
+
+        let users = usecase::get_users_by_ids(self.ctx(), ids)
+            .await
+            .map_err(|e| handle_error(e))?;
+
+        Ok(Response::new(users.into()))
+    }
+}
+
+fn handle_error(err: anyhow::Error) -> Status {
+    eprintln!("{err:?}");
+
+    match err.downcast_ref::<AppError>() {
+        Some(err) => match err {
+            AppError::InvalidArgument(msg) => Status::invalid_argument(msg),
+            AppError::NotFound(msg) => Status::not_found(msg),
+            AppError::Internal(msg) => Status::internal(msg),
+        },
+        None => Status::internal("internal error"),
     }
 }
