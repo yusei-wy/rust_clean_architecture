ef09a2b2b5766cffa82d1336ea45e1c8cc0ce764
yusei-wy <yusei.kasa@gmail.com>
2022-11-03 08:11:11 +0000

    add: application endpoint

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use app_context::AppContext;
+use diesel::r2d2::ConnectionManager;
+use grpc_handler::{user::v1::user_service_server::UserServiceServer, UserServiceHandler};
+use psql_repository::UserRepository;
+use tonic::transport::Server;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let database_url =
+        std::env::var("DATABASE_URL").expect("failed to read the env var DATABASE_URL");
+    let manager = ConnectionManager::new(database_url);
+    let pool = r2d2::Pool::new(manager).expect("failed to create the connection pool");
+
+    let user_repository = UserRepository::new(pool);
+
+    let context = AppContext { user_repository };
+
+    let user_service = UserServiceHandler::new(context);
+
+    let add = "[::1]:50051".parse()?;
+    println!("Start sample app server!");
+
+    Server::builder()
+        .add_service(UserServiceServer::new(user_service))
+        .serve(add)
+        .await?;
+
+    Ok(())
 }
